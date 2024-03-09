2fa7ece0078b641eca5d7694e8dcd734179cee28
Doug Forbes <douglaskforbes@gmail.com>
2024-03-09 16:04:55 +0000

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,24 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins, Space_Grotesk, Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import { urlToHttpOptions } from "url";
 
 const inter = Inter({ subsets: ["latin"] });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  variable: '--font-spaceGrotesk'
+});
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-mono",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${poppins.variable} ${roboto_mono.variable} ${spaceGrotesk.variable}`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        poppins: ["var(--font-poppins)", "sans-serif"],
+        spaceGrotesk: ["var(--font-spaceGrotesk)", "sans-serif"],
+        mono: ["var(--font-roboto-mono)"],
       },
     },
   },
